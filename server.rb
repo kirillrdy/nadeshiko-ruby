ecc8219f09f093ba3caf88227a5bf7174cded9cf
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-08-07 13:57:10 +1000

    Server now connects to mysql for ActiveRecord

--- server.rb
+++ server.rb
@@ -5,18 +5,25 @@ require 'digest/sha1'
 
 #Digest::SHA1.hexdigest 'foo'
 
-elements = ['element','button','textfield','list']
+elements = ['element','button','textfield','list','grid']
 
 elements.each{|x| require_relative 'lib/elements/'+x }
 
 require_relative 'lib/dom_on_sockets'
 require_relative 'lib/app_sprawn'
 require_relative 'lib/app'
-require_relative 'store'
+require_relative 'stores/movie_store'
 require_relative 'myapp'
+
+
+require 'sinatra/activerecord'
+ActiveRecord::Base.establish_connection :host => 'localhost', :database => 'dom_on_sockets', :adapter => 'mysql2'
+require_relative 'models/movie'
+
 
 EventMachine.run do
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
     AppSprawn.setup ws
   end
+  puts "Server started"
 end
