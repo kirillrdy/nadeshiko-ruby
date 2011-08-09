0e02868dc3208a36a3f50e83a6a2c992272867d9
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-08-10 09:49:41 +1000

    Ported DSL

---
+++ lib/dsl.rb
@@ -0,0 +1,35 @@
+module Dsl
+
+  attr_accessor :children, :method
+
+  def method_missing method, *args, &block
+    a = Node.new(method)
+    a.instance_eval(&block) if block_given?
+    @children ||= []
+    @children << a
+  end
+  
+end
+
+class Node
+  include Dsl
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+p = Node.new :root
+
+p.instance_eval do
+  hbox do
+    vbox do
+      text 'a'
+      text 'b'
+      button 'hello'
+    end
+    button 'hello'
+  end
+end
+
+puts p.inspect
