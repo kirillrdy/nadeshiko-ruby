cef136d317499c826c18586ebb8bf8c2769cdcec
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-08-11 17:32:39 +1000

    Reorganized everything

--- server.rb
+++ server.rb
@@ -1,29 +1,27 @@
 require 'em-websocket'
 require 'json'
-
 require 'digest/sha1'
 
-#Digest::SHA1.hexdigest 'foo'
 
 elements = ['element','button','textfield','list','grid']
 
+require_relative 'lib/dsl'
 elements.each{|x| require_relative 'lib/elements/'+x }
 
-require_relative 'lib/dom_on_sockets'
-require_relative 'lib/app_sprawn'
-require_relative 'lib/app'
+libs = ['dom_on_sockets','app','activerecord']
+libs.each{|x| require_relative 'lib/'+ x}
+
 require_relative 'stores/movie_store'
+
 require_relative 'myapp'
 
-
-require 'sinatra/activerecord'
-ActiveRecord::Base.establish_connection :host => 'localhost', :database => 'dom_on_sockets', :adapter => 'mysql2'
-require_relative 'models/movie'
-
-
 EventMachine.run do
-  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
-    AppSprawn.setup ws
+  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |web_socket|
+    web_socket.onopen do
+      dom_on_sockets = DomOnSockets.new(web_socket)
+      app = MyApp.new dom_on_sockets
+      app.setup_app
+    end
   end
   puts "Server started"
 end
