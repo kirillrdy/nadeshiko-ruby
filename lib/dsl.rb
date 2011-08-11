649cda9318ed93cb7013046c0ee21c65939004d3
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-08-11 18:24:09 +1000

    undid dsl

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -2,8 +2,8 @@ module Dsl
 
   attr_accessor :children, :method
 
-  def method_missing method, *args, &block
-    a = Node.new(method,*args)
+  def method_missing method,*args, &block
+    a = Node.new(method)
     a.instance_eval(&block) if block_given?
     @children ||= []
     @children << a
@@ -19,17 +19,17 @@ class Node
 end
 
 
-#p = Node.new :root
+p = Node.new :root
 
-#p.instance_eval do
-#  hbox do
-#    vbox do
-#      text 'a'
-#      text 'b'
-#      button 'hello'
-#    end
-#    button 'hello'
-#  end
-#end
+p.instance_eval do
+  hbox do
+    vbox do
+      text 'a'
+      text 'b'
+      button 'hello'
+    end
+    button 'hello'
+  end
+end
 
-#puts p.inspect
+puts p.inspect
