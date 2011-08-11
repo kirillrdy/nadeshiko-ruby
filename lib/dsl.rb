1f294e6d45763d2d9315d9bb26113d556404625a
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-08-11 17:37:37 +1000

    commented out code thats only indicates example of usage and made DSL passing down arguments

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -3,7 +3,7 @@ module Dsl
   attr_accessor :children, :method
 
   def method_missing method, *args, &block
-    a = Node.new(method)
+    a = Node.new(method,*args)
     a.instance_eval(&block) if block_given?
     @children ||= []
     @children << a
@@ -19,17 +19,17 @@ class Node
 end
 
 
-p = Node.new :root
+#p = Node.new :root
 
-p.instance_eval do
-  hbox do
-    vbox do
-      text 'a'
-      text 'b'
-      button 'hello'
-    end
-    button 'hello'
-  end
-end
+#p.instance_eval do
+#  hbox do
+#    vbox do
+#      text 'a'
+#      text 'b'
+#      button 'hello'
+#    end
+#    button 'hello'
+#  end
+#end
 
-puts p.inspect
+#puts p.inspect
