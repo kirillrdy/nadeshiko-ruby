a1cbcf3b209d5c42455fe2a37e55b2fe6460ca37
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-08-14 15:44:05 +1000

    Added Dialog and abstracted out styles

--- myapp.rb
+++ myapp.rb
@@ -2,21 +2,30 @@ class MyApp < App
 
   def onstart
 
-    #@dom_on_sockets.set_css_by_selector 'body','background-color','#333'
+    # Setting body style
+    @dom_on_sockets.set_css_by_selector 'body','background-color','#9aa'
     @dom_on_sockets.set_css_by_selector 'body','padding','0px'
     @dom_on_sockets.set_css_by_selector 'body','margin','0px'
+    # Theme presets
+    top_heading = { :padding => '5px',
+                  'margin-top' => '0px',
+                  :background => '-moz-linear-gradient(top, #1e5799 0%, #2989d8 50%, #207cca 51%, #7db9e8 100%)'
+                  }
 
-    main_style = { :margin => :auto, :border => '1px solid black',  :width => '1000px',
-                'background-color' => '#eee', 'border-radius'=> '5px', 'margin-top' => '5px' }
+    background = {'background-color' => '#eee'}
+    border = {:border => '1px solid black'}
+    round_top = {'border-radius'=> '5px'}
 
-    header_style = { :height => '80px','border-radius-top'=> '5px', :padding => '5px',
-          #:background => '-moz-linear-gradient(top, #7abcff 0%,#60abf8 44%,#4096ee 100%)'
-          :background => '-moz-linear-gradient(top, #1e5799 0%, #2989d8 50%, #207cca 51%, #7db9e8 100%)' }
+    main_style = { :margin => :auto, :width => '1000px','margin-top' => '5px' }.merge(background).merge(border).merge(round_top)
+    header_style = top_heading.merge({ :height => '80px' })
+    dialog_style = {:position => 'absolute'}.merge(background).merge(border).merge(round_top)
+    titles_style = {'text-shadow' => '#444 2px -2px 2px', :color => 'white'}
+
 
     add_elements do
       div :id => :main, :style => main_style do
         div :style => header_style do
-          h1 :text => 'Eiga', :style => {'text-shadow' => '#444 2px -2px 2px', :color => 'white'}
+          h1 :text => 'Eiga', :style => titles_style
         end
         div :style => { :padding => '10px' } do
           input :id => :textfield
@@ -26,6 +35,32 @@ class MyApp < App
                 :columns => [:id, :title]
         end
       end
+
+      div :id => :dialog, :style => dialog_style do
+        h4 :id => :dialog_header, :text => 'Dialog', :style => top_heading.merge(titles_style)
+      end
+
+    end
+
+
+    @dom_on_sockets.get_screen_size do |width,height|
+      dialog = get_element :dialog
+
+      dialog_width = 400
+      dialog_height = 300
+      screen_width = width.to_i
+      screen_heigth = height.to_i
+      
+      left = (screen_width / 2) - ( dialog_width / 2)
+      top = (screen_heigth / 2) - ( dialog_height / 2)
+
+      dialog.set_css 'width',dialog_width
+      dialog.set_css 'height',dialog_height
+      dialog.set_css 'left',left
+      dialog.set_css 'top',top
+
+      dialog.make_draggable :dialog_header
+
     end
 
     button = get_element :add_new_record
