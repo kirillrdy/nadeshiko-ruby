4b4b7d06c190c5346927c87bc4a90763ab4f177f
Kirill <kirillrdy@gmail.com>
2011-08-25 17:55:42 +1000

    Using bundler

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+gem 'em-websocket'
+gem 'activerecord'
+gem 'mysql2', '< 0.3.0'
+#gem 'sinatra-activerecord'
+#gem 'activerecord-mysql2-adapter'
+
--- lib/activerecord.rb
+++ lib/activerecord.rb
@@ -1,3 +1,4 @@
-require 'sinatra/activerecord'
+#require 'sinatra/activerecord'
+require 'active_record'
 ActiveRecord::Base.establish_connection :host => 'localhost', :database => 'dom_on_sockets', :adapter => 'mysql2'
 require_relative '../app/models/movie'
