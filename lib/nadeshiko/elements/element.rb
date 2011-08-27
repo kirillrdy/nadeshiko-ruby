8136951119f1b3f414897342405fafa4e858dd83
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-08-27 13:13:30 +1000

    Added span to list of basic elements

--- lib/nadeshiko/elements/element.rb
+++ lib/nadeshiko/elements/element.rb
@@ -34,7 +34,7 @@ class Element
 
   def method_missing element_type,*args, &block
 
-    html_based_elements = [:h1,:div,:input,:button,:table,:tr,:th,:td,:thead,:tbody,:h4]
+    html_based_elements = [:h1,:div,:input,:button,:table,:tr,:th,:td,:thead,:tbody,:h4,:span]
     magic_based_elements = [:grid2]
 
     super unless (html_based_elements + magic_based_elements).include? element_type
