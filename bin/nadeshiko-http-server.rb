fba28bbc2d86454328c4bf354825045bfd36698f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-08-31 12:03:08 +1000

    Nadeshiko http server now can use per project public folders

--- bin/nadeshiko-http-server.rb
+++ bin/nadeshiko-http-server.rb
@@ -4,10 +4,26 @@ require 'sinatra'
 
 public_dir = File.dirname(__FILE__)+ '/../public'
 
-set :public, public_dir
+#set :public, public_dir
+
+set :root, Dir.getwd
 set :run, true
 
 
 get '/' do
   IO.read  public_dir + '/index.html'
+end
+
+list_of_javascript_files = ['dom_crud.js',
+  'FABridge.js',
+  'jquery.min.js',
+  'jquery-ui.min.js',
+  'swfobject.js',
+  'web_socket.js'
+  ]
+
+list_of_javascript_files.each do |file|
+  get '/javascripts/'+ file do
+    IO.read public_dir + '/javascripts/' + file
+  end
 end
