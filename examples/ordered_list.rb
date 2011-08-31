ec8ec648d4727ed2e7140d9c062d8394d7876763
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-08-31 14:23:23 +1000

    no can add new entries to list

--- examples/ordered_list.rb
+++ examples/ordered_list.rb
@@ -1,34 +1,43 @@
 require './lib/nadeshiko'
 
-class Notifier
-  def self.onmove &block
-    @onmove ||= []
-    @onmove << block
-  end
-  
-  def self.move element_id, position
-    @onmove.each{|x| x.call element_id,position }
-  end
-  
-end
-
 class HelloWord < Nadeshiko::Application
 
   def onstart
+    input :id => 'textfield'
+    button :id => 'add_item', :text => 'add'
+
     ul :id => 'sortable' do
-      ['item1','item2','item3','item4'].each do |i|
-        li :id => i do
+      ['item1','item2','item3'].each do |i|
+        li do
           span :text => i
         end
       end
     end
 
-    Notifier.onmove do |item,prev_item|
-      get_element(item).insert_after(get_element(prev_item))
+
+    # subscribe to notifications
+    Nadeshiko::Notifier.notify_on :item_moved do |item,prev_item|
+      e = get_element(item)
+      e.insert_after(get_element(prev_item)) if e
     end
 
+    Nadeshiko::Notifier.notify_on :item_added do |value|
+      add_elements_to(:sortable) do
+        li do
+          span :text => value
+        end
+      end
+    end
+
+    # Hook into events
     get_element(:sortable).sortable do |item,prev_item|
-      Notifier.move item,prev_item
+      Nadeshiko::Notifier.trigger :item_moved,item,prev_item
+    end
+
+    get_element(:add_item).onclick do
+      get_element(:textfield).get_value do |value|
+        Nadeshiko::Notifier.trigger :item_added, value
+      end
     end
 
   end
