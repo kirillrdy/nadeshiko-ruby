d39876d265ce2862b39ce7976a24aa790a504d67
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-02 12:35:21 +1000

    Automagically set create_order

--- examples/issue_tracker/models/issue.rb
+++ examples/issue_tracker/models/issue.rb
@@ -1,2 +1,7 @@
 class Issue < ActiveRecord::Base
+  default_scope order(:sort_order)
+  before_create do
+    last_issue = Issue.order(:sort_order).last
+    self.sort_order = last_issue ? last_issue.sort_order + 1 : 0
+  end
 end
