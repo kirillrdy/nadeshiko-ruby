7749c971b3254ec1056fea73f8125f2107eb70e7
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-02 10:41:48 +1000

    Working on complete version of updated list

--- examples/ordered_list.rb
+++ examples/ordered_list.rb
@@ -8,7 +8,7 @@ class HelloWord < Nadeshiko::Application
 
     ul :id => 'sortable' do
       ['item1','item2','item3'].each do |i|
-        li do
+        li :id => i do
           span :text => i
         end
       end
@@ -16,9 +16,14 @@ class HelloWord < Nadeshiko::Application
 
 
     # subscribe to notifications
-    Nadeshiko::Notifier.notify_on :item_moved do |item,prev_item|
+    Nadeshiko::Notifier.notify_on :item_moved_to_top do |item,next_element_id|
       e = get_element(item)
-      e.insert_after(get_element(prev_item)) if e
+      e.insert_after(get_element(next_element_id))
+    end
+
+    Nadeshiko::Notifier.notify_on :item_moved do |item,prev_element_id|
+      e = get_element(item)
+      e.insert_after(get_element(prev_element_id))
     end
 
     Nadeshiko::Notifier.notify_on :item_added do |value|
@@ -30,8 +35,22 @@ class HelloWord < Nadeshiko::Application
     end
 
     # Hook into events
-    get_element(:sortable).sortable do |item,prev_item|
-      Nadeshiko::Notifier.trigger :item_moved,item,prev_item
+    get_element(:sortable).sortable
+
+    get_element(:sortable).sortupdate do |item_id|
+      moved_element = get_element(item_id.to_sym)
+
+      moved_element.index do |index|
+        if index.to_i == 0
+          moved_element.next do |next_element_id|
+            Nadeshiko::Notifier.trigger :item_moved_to_top,moved_element,next_element_id
+          end
+        else
+          moved_element.prev do |prev_element_id|
+            Nadeshiko::Notifier.trigger :item_moved,moved_element,prev_element_id
+          end
+        end
+      end
     end
 
     get_element(:add_item).click do
