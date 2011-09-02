69a03189ddd8051b0cb9f27cf5c6e63ea0e754a1
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-02 16:39:26 +1000

    More Message Batching

--- examples/issue_tracker/issue_tracker.rb
+++ examples/issue_tracker/issue_tracker.rb
@@ -49,18 +49,22 @@ class IssueTracker < Nadeshiko::Application
     div :id => :footer, :text => "Powered by Nadeshiko #{Nadeshiko::VERSION}"
 
     Nadeshiko::Notifier.notify_on :issue_create do |issue|
-      add_elements_to(:list_of_issues) do
-        add_issue_to_list issue
+      batch_messages do
+        add_elements_to(:list_of_issues) do
+          add_issue_to_list issue
+        end
       end
     end
 
     Nadeshiko::Notifier.notify_on :issue_moved do |moved_issue_element_id,target_id,special_case|
       moved_element = get_element(moved_issue_element_id)
       target = get_element(target_id)
-      if special_case
-        moved_element.insert_before(target)
-      else
-        moved_element.insert_after(target)
+      batch_messages do
+        if special_case
+          moved_element.insert_before(target)
+        else
+          moved_element.insert_after(target)
+        end
       end
     end
 
