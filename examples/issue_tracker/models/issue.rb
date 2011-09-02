fcb9aa1c819dce00592ab58c0c749ec7b491bb17
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-02 14:24:21 +1000

    After Destroy of Issue recalculate sort_order

--- examples/issue_tracker/models/issue.rb
+++ examples/issue_tracker/models/issue.rb
@@ -4,4 +4,14 @@ class Issue < ActiveRecord::Base
     last_issue = Issue.order(:sort_order).last
     self.sort_order = last_issue ? last_issue.sort_order + 1 : 0
   end
+
+  after_destroy do
+    Issue.transaction do
+      Issue.where(['sort_order > ?',self.sort_order]).each do |x|
+        x.sort_order -= 1
+        x.save
+      end
+    end
+  end
+
 end
