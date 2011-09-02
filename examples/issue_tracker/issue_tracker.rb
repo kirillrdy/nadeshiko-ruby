84b2f8d7284ff1eb74989ad7f5582abd72a3ee86
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-02 13:57:56 +1000

    Fully Realtime Issue Tracker !!!

--- examples/issue_tracker/issue_tracker.rb
+++ examples/issue_tracker/issue_tracker.rb
@@ -12,49 +12,81 @@ class IssueTracker < Nadeshiko::Application
       end
     end.sortable
 
+
+    Nadeshiko::Notifier.notify_on :issue_create do |issue|
+      add_elements_to(:list_of_issues) do
+        add_issue_to_list issue
+      end
+    end
+
+    Nadeshiko::Notifier.notify_on :issue_moved do |moved_issue_element_id,target_id,special_case|
+      moved_element = get_element(moved_issue_element_id)
+      target = get_element(target_id)
+      if special_case
+        moved_element.insert_before(target)
+      else
+        moved_element.insert_after(target)
+      end
+    end
+
     get_element(:add_new_issue_button).click do
       get_element(:new_issue_text_field).val do |value|
+        get_element(:new_issue_text_field).val ''
         issue = Issue.new :description => value
         issue.save!
-        add_elements_to(:list_of_issues) do
-          add_issue_to_list issue
-        end
+        Nadeshiko::Notifier.trigger :issue_create, issue
       end
     end
+
 
     get_element(:list_of_issues).sortupdate do |moved_element_id|
-      puts moved_element_id
-      moved_element = get_element(moved_element_id)
-      moved_element.index do |index|
-        issue = moved_element.options[:record]
-
-        issue.reload
-
-        old_pos = issue.sort_order
-        new_pos = index.to_i
-        
-        range = old_pos > new_pos ? (new_pos..old_pos-1) : (old_pos..new_pos)
-
-        sort_orders = range.to_a
-
-        puts "moving #{old_pos} to #{new_pos}"
-        puts "will search for sort_order #{sort_orders}"
-        issues_to_update = Issue.where(:sort_order => sort_orders )
-        puts issues_to_update.map{|x| "#{x.description},#{x.sort_order}" }
-
-        if old_pos > new_pos
-          issues_to_update.each{|x| x.sort_order +=1 ; x.save! }
-        else
-          issues_to_update.each{|x| x.sort_order -=1 ; x.save! }
-        end
-
-        puts "setting new position to #{new_pos}"
-        issue.sort_order = new_pos
-        issue.save!
-
-      end
+      move_element moved_element_id
     end
 
+  end
+
+  def move_element moved_element_id
+
+
+
+
+    moved_element = get_element(moved_element_id)
+    moved_element.index do |index|
+      issue = moved_element.options[:record]
+      issue.reload
+
+      old_pos = issue.sort_order
+      new_pos = index.to_i
+
+      range = old_pos > new_pos ? (new_pos..old_pos-1) : (old_pos..new_pos)
+      sort_orders = range.to_a
+      issues_to_update = Issue.where(:sort_order => sort_orders )
+
+      if old_pos > new_pos
+        issues_to_update.each{|x| x.sort_order +=1 ; x.save! }
+      else
+        issues_to_update.each{|x| x.sort_order -=1 ; x.save! }
+      end
+
+      issue.sort_order = new_pos
+      issue.save!
+
+      # Notification thingy
+      moved_element.index do |index|
+        if index.to_i == 0
+          moved_element.next do |next_id|
+            Nadeshiko::Notifier.trigger :issue_moved, moved_element_id,next_id, true
+          end
+        else
+          moved_element.prev do |prev_id|
+            Nadeshiko::Notifier.trigger :issue_moved, moved_element_id,prev_id, false
+          end
+        end
+      end
+
+      
+
+    end
   end
 
   def add_issue_to_list issue
