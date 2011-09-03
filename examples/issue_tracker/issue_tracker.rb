8e406519cd4367b75593865beb6d9f85c0751252
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-03 12:54:07 +1000

    Better styling( started using bootstrap)

--- examples/issue_tracker/issue_tracker.rb
+++ examples/issue_tracker/issue_tracker.rb
@@ -1,50 +1,84 @@
 require_relative 'boot'
 class IssueTracker < Nadeshiko::Application
-  def onstart
 
-    style do
-      {
-        '#list_of_issues' => {
-          :height => '80%',
-          :width => '400px',
-          #:border => '1px solid gray'
-          :background => '#efe',
-          'border-radius' => '3px',
-        },
-        '#footer' => {
-          :margin => :auto,
-          :width => '200px'
-        },
-        '.issue' => {
-          #:width => '400px',
-          :width => '100%',
-          'border-radius' => '3px',
-          :border => '1px solid gray',
-          :background => '#eee'
-        },
-        '.issue-description' => {
-          :width => '330px'
-        },
-        '.issue-buttons' => {
-          'float' => 'right'
-        }
-      }
-    end
-
-    h1 :text => 'Non-pivotal tracker (TM)'
-    div do
-      input :id => :new_issue_text_field
-      button :id => :add_new_issue_button, :text => 'Add New Issue'
-    end
-
-    span :text => 'Icebox'
+  def icebox_panel
+    h4 :text => 'Icebox'
     div :id => :list_of_issues do
       Issue.all.each do |issue|
         add_issue_to_list issue
       end
     end.sortable
+  end
+
+  def onstart
+
+    append '<link rel="stylesheet" href="http://twitter.github.com/bootstrap/assets/css/bootstrap-1.2.0.min.css">'
+
+    style do
+      {
+        'issue-description' => {
+          :width => '300px'
+        },
+        '.float-right' => {
+          :float => 'right'
+        }
+      }
+    end
+
+#    style do
+#      {
+#        '#list_of_issues' => {
+#          :height => '80%',
+#          :width => '400px',
+#          #:border => '1px solid gray'
+#          :background => '#efe',
+#          'border-radius' => '3px',
+#        },
+#        '#footer' => {
+#          :margin => :auto,
+#          :width => '200px'
+#        },
+#        '.issue' => {
+#          #:width => '400px',
+#          :width => '100%',
+#          'border-radius' => '3px',
+#          :border => '1px solid gray',
+#          :background => '#eee'
+#        },
+#        '.issue-description' => {
+#          :width => '330px'
+#        },
+#        '.issue-buttons' => {
+#          'float' => 'right'
+#        }
+#      }
+#    end
+
+    h1 :text => 'Non-pivotal tracker (TM)'
+
+
+    div :class => 'row show-grid' do
+      div :class => 'span8 column' do
+        div do
+          input :id => :new_issue_text_field, :class => 'xlarge'
+          button :id => :add_new_issue_button, :text => 'Add New Issue', :class => 'btn primary'
+        end
+        icebox_panel
+      end
+      div :class => 'span8 column' do
+        input :id => :search_issue_text_field
+        button :id => :search_issue_button, :text => 'Search', :class => 'btn'
+      end
+      div :class => 'span8 column' do
+        span :text => 'other things here'
+        #icebox_panel
+      end
+    end
 
     div :id => :footer, :text => "Powered by Nadeshiko #{Nadeshiko::VERSION}"
+
+
+
 
     Nadeshiko::Notifier.notify_on :issue_create do |issue|
       batch_messages do
@@ -130,16 +164,20 @@ class IssueTracker < Nadeshiko::Application
   end
 
   def add_issue_to_list issue
-    issue_div = div :id => "issue_#{issue.id}", :record => issue, :class => 'issue' do
-      
+    issue_div = div :id => "issue_#{issue.id}", :record => issue, 
+      #:class => "alert-message block-message warning" do
+      :class => "well" do
+
       story_description = "#{issue.id}: #{issue.description}"
-      
-      span :text => story_description, :class => 'issue-description'
-      x = div :text => 'x', :class => 'issue-buttons'
-      x.click do
-        issue.destroy
-        Nadeshiko::Notifier.trigger :issue_destroyed, issue_div.id
-        #issue_div.remove
+
+      div :text => story_description, :class => 'issue-description'
+      div :class => 'alert-actions' do
+        span :text => 'Finish', :class => 'btn success'
+        x = span :text => 'Delete', :class => 'btn danger'
+        x.click do
+          issue.destroy
+          Nadeshiko::Notifier.trigger :issue_destroyed, issue_div.id
+        end
       end
     end
   end
