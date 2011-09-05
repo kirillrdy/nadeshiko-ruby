030460c9e8ec20eaa51430c506f6c4f51ae8035f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-05 11:13:38 +1000

    removed callback

--- examples/issue_tracker/app/models/issue.rb
+++ examples/issue_tracker/app/models/issue.rb
@@ -1,9 +1,10 @@
 class Issue < ActiveRecord::Base
   default_scope order(:sort_order)
-  before_create do
-    last_issue = Issue.order(:sort_order).last
-    self.sort_order = last_issue ? last_issue.sort_order + 1 : 0
-  end
+
+#  before_create do
+#    last_issue = Issue.order(:sort_order).last
+#    self.sort_order = last_issue ? last_issue.sort_order + 1 : 0
+#  end
 
   after_destroy do
     Issue.transaction do
