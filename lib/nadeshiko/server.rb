124ca27d534a3e0eaf250b5ac5c1329a68d505a9
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-05 14:02:57 +1000

    New merged server

--- lib/nadeshiko/server.rb
+++ lib/nadeshiko/server.rb
@@ -1,4 +1,37 @@
+require 'sinatra'
+require 'thin'
+
 module Nadeshiko
+
+  class HttpServer < Sinatra::Base
+
+    public_dir = File.dirname(__FILE__)+ '/../../public'
+
+    #set :public, public_dir
+
+    set :root, Dir.getwd
+    set :run, true
+
+
+    get '/' do
+      IO.read  public_dir + '/index.html'
+    end
+
+    list_of_javascript_files = ['dom_crud.js',
+      'FABridge.js',
+      'jquery.min.js',
+      'jquery-ui.min.js',
+      'swfobject.js',
+      'web_socket.js'
+      ]
+
+    list_of_javascript_files.each do |file|
+      get '/javascripts/'+ file do
+        IO.read public_dir + '/javascripts/' + file
+      end
+    end
+
+  end
 
   class Server
 
@@ -12,7 +45,7 @@ module Nadeshiko
             app.start
           end
         end
-        puts "Nadeshiko Server started"
+        HttpServer.run!({:port=>8000})
       end
 
     end
