fb3a13467ba43cf80bfe6c8f5d867e563ad84f7e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-05 11:12:09 +1000

    Small formattign issues and message batching

--- examples/issue_tracker/app/event_handers.rb
+++ examples/issue_tracker/app/event_handers.rb
@@ -1,14 +1,16 @@
 module EventHandlers
   def issue_events
 
-      get_element(:new_issue_text_field).keydown do |key|
+    get_element(:new_issue_text_field).keydown do |key|
       if key.to_i == 13
         create_new_issue
       end
     end
 
     get_element(:add_new_issue_button).click do
-      create_new_issue
+      batch_messages do
+        create_new_issue
+      end
     end
 
 
