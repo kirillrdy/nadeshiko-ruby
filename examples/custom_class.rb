21f1ad2b935beac1deb9cdeba5d37c4330c408ee
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-06 10:24:58 +1000

    Added example that demoes how to add custom classes

---
+++ examples/custom_class.rb
@@ -0,0 +1,27 @@
+require './lib/nadeshiko'
+
+module Nadeshiko
+  class List < Element
+
+    def initialize options = {}
+      super
+      @element_type = :div
+    end
+
+    def setup
+      text 'I am custom class'
+    end
+
+  end
+end
+
+class CustomClass < Nadeshiko::Application
+
+  def onstart
+    span :text => 'Hello World'
+    list :id => :custom_list1
+  end
+
+end
+
+Nadeshiko::Server.run CustomClass
