a79a4b1d6b2f9a542a1d10502cddc202f5e39bcb
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-09-07 12:27:34 +1000

    Cleaned up issue, and added ordered loading

--- examples/issue_tracker/app/models/issue.rb
+++ examples/issue_tracker/app/models/issue.rb
@@ -1,26 +1,11 @@
 class IssueTracker < Nadeshiko::Application
-class Issue < ActiveRecord::Base
-  default_scope order(:sort_order)
+  class Issue < ActiveRecord::Base
 
-#  before_create do
-#    last_issue = Issue.order(:sort_order).last
-#    self.sort_order = last_issue ? last_issue.sort_order + 1 : 0
-#  end
-
-  after_destroy do
-    Issue.transaction do
-      Issue.where(['sort_order > ?',self.sort_order]).each do |x|
-        x.sort_order -= 1
-        x.save
+    def self.load_in_order ids
+      self.transaction do
+        return ids.map{|x| self.find x }
       end
     end
-  end
 
-  def self.load_in_order ids
-    self.transaction do
-      return ids.map{|x| self.find x }
-    end
   end
-
-end
 end
