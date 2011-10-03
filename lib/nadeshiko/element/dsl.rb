08f45417ad81988bf3cd5e7d4f17b10aa6977f97
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-10-03 17:05:00 +1100

    Added link to a list of allowed elements

--- lib/nadeshiko/element/dsl.rb
+++ lib/nadeshiko/element/dsl.rb
@@ -8,7 +8,7 @@ module Nadeshiko
       options.merge!({:app => app, :element_type => element_type})
 
       html_based_elements = [:h1,:h2,:h3,:h4,:div,:input,:button,
-        :table,:tr,:th,:td,:thead,:tbody,:span,
+        :table,:tr,:th,:td,:thead,:tbody,:span,:a,
         :ul,:li,:link]
 
       if html_based_elements.include? element_type
