5cdc63fdcea327df7006ab692a57053d159a7f00
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2011-12-18 16:28:43 +1100

    Updated to new bootstrap css and fixed indentation

--- examples/issue_tracker/app/styling.rb
+++ examples/issue_tracker/app/styling.rb
@@ -1,34 +1,34 @@
 class IssueTracker < Nadeshiko::Application
-module Styling
-  def issues_styling
-    append '<link rel="stylesheet" href="http://twitter.github.com/bootstrap/assets/css/bootstrap-1.2.0.min.css">'
+  module Styling
+    def issues_styling
+      append '<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">'
 
-    style do
-      {
-        'body' => {
-          'padding-top' => '40px'
-        },
-        '.issue-description' => {
-          :width => '260px'
-        },
-        '.right' => {
-          :float => 'right'
-        },
-        '.left' => {
-          :float => 'left'
-        },
-        '.list-of-issues' => {
-          background:  '#eee',
-          height: '800px',
-          overflow: 'auto',
-          'border-radius' => '6px'
-        },
-        '.well' => {
-          'margin-bottom' =>  '1px'
+      style do
+        {
+          'body' => {
+            'padding-top' => '40px'
+          },
+          '.issue-description' => {
+            :width => '260px'
+          },
+          '.right' => {
+            :float => 'right'
+          },
+          '.left' => {
+            :float => 'left'
+          },
+          '.list-of-issues' => {
+            background:  '#eee',
+            height: '800px',
+            overflow: 'auto',
+            'border-radius' => '6px'
+          },
+          '.well' => {
+            'margin-bottom' =>  '1px'
+          }
+
         }
-
-      }
+      end
     end
   end
-end
 end
