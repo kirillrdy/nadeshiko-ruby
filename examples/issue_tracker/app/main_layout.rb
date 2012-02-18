f59299723512f88925806bba7d5aa4959da34cb3
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-02-18 15:38:58 +1100

    changed title, added clock and user name

--- examples/issue_tracker/app/main_layout.rb
+++ examples/issue_tracker/app/main_layout.rb
@@ -6,13 +6,19 @@ class Silverforge < Nadeshiko::Application
       div :class => "navbar" do
         div :class => "navbar-inner" do
           div :class => "container" do
-            a :class => 'brand', :text => 'Non-pivotal tracker (TM)'
+            a :class => 'brand', :text => 'Silverforge'
             ul :class => :nav do
               li :id => :home_li, :class => :active do
                 a :text => 'Home', :id => :home_section_link
               end
-              li :id => :admin_li do
+              li :id => :admin_li, :class => 'span6' do
                 a :text => 'Admin', :id => :admin_section_link
+              end
+              li do
+                a :text => '', :id => :current_user_name
+              end
+              li do
+                a :text => 'clock', :id => :clock_link
               end
             end
           end
@@ -24,6 +30,13 @@ class Silverforge < Nadeshiko::Application
       end
 
       div :id => :footer, :text => "Powered by Nadeshiko #{Nadeshiko::VERSION}"
+
+
+      EventMachine::PeriodicTimer.new(1) do
+        get_element(:clock_link).text Time.now.strftime('%H:%M:%S')
+      end
+
+
     end
 
     def nav_bar_events
