593008d79205d4b6375504edf531db498ee3a8bf
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-02-18 13:59:12 +1100

    took out admin stuff into Admin Module

---
+++ examples/issue_tracker/app/admin.rb
@@ -0,0 +1,32 @@
+class IssueTracker < Nadeshiko::Application
+
+  module Admin
+
+    def add_admin_layout
+      append_to :main_body do
+        table :class => 'table table-striped' do
+          thead do
+            tr do
+              th :text => 'Id'
+              th :text => 'description'
+              th :text => 'Actions'
+            end
+          end
+          tbody do
+            for issue in Issue.all
+              tr do
+                td :text => issue.id
+                td :text => issue.description
+                td do
+                  div :text => 'Delete', :class => 'btn'
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+  end
+
+end
