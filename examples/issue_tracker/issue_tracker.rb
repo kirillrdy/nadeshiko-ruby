9e61300ee1f1be72fe3edadd770cbbd1f615481c
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-02-18 14:00:46 +1100

    include Admin and Login modules

--- examples/issue_tracker/issue_tracker.rb
+++ examples/issue_tracker/issue_tracker.rb
@@ -3,10 +3,12 @@ require_relative 'boot'
 
 class IssueTracker < Nadeshiko::Application
 
+  include LoginScreen
   include Styling
   include EventHandlers
   include Layout
   include Methods
+  include Admin
 
   def onstart
     issues_styling
