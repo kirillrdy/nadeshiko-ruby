c0eb40424bf6227b7743209581b2b0d5f7d27e6a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-02-18 14:00:14 +1100

    opse, create Task instead of Issue

--- examples/issue_tracker/app/methods.rb
+++ examples/issue_tracker/app/methods.rb
@@ -5,7 +5,7 @@ class IssueTracker < Nadeshiko::Application
       get_element(:new_issue_text_field).val do |value|
         if value != ''
           get_element(:new_issue_text_field).val ''
-          issue = Issue.new :description => value
+          issue = Task.new :description => value
           issue.save!
           get_element(:icebox).append_record issue, :notify => true
         end
