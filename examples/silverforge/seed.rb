97687a9a38f072c3ad3f42c654acc8dcb1d897bb
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-02-20 09:08:10 +1100

    updated seeds

--- examples/silverforge/seed.rb
+++ examples/silverforge/seed.rb
@@ -1,4 +1,19 @@
 require_relative 'boot'
 
-Silverforge::User.create :email => 'kirillrdy@gmail.com', :name => 'Kirill Radzikhovskyy'
+class Silverforge < Nadeshiko::Application
+
+  silverpond_account  = Account.new :name => 'Silverpond'
+  silverpond_account.save!
+
+  kirill = User.new :email => 'kirillrdy@silverpond.com.au', :name => 'Kirill Radzikhovskyy'
+  kirill.save!
+
+  silverpond_account.users << kirill
+
+  silverforge = silverpond_account.projects.new :name => 'Silverforge'
+  silverforge.users << kirill
+
+
+end
+
 
