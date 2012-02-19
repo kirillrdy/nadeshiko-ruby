a93a89c8616aecc252c83c40e67b8b88bff98efd
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-02-20 09:28:21 +1100

    added current work item thingo

--- examples/silverforge/app/models/user.rb
+++ examples/silverforge/app/models/user.rb
@@ -5,8 +5,19 @@ class Silverforge < Nadeshiko::Application
 
     has_many :task_logs
 
+    def start_task task
+      @current_item_description = task.description
+      @current_item_start_time = Time.now
+    end
+
+    def current_work_item
+      return "Doing Nothing ..." if @current_item_description.nil?
+      "#{@current_item_description} #{(Time.now - @current_item_start_time).to_i}"
+    end
+
     def self.authenticate email
       self.where(:email => email).first
     end
+
   end
 end
