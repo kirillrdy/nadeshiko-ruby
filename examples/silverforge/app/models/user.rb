4097fd526e30452c1cd17e606c6574365440c530
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2012-02-19 19:34:18 +1100

    added relationships to user

--- examples/silverforge/app/models/user.rb
+++ examples/silverforge/app/models/user.rb
@@ -1,5 +1,7 @@
 class Silverforge < Nadeshiko::Application
   class User < ActiveRecord::Base
+    has_many :project_users
+    has_many :projects, :through => :project_users
     def self.authenticate email
       self.where(:email => email).first
     end
